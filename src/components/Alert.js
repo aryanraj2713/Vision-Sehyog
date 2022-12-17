173e713852c55b9b4437cb4f21675f0622223b29
Aryan Raj <aryanraj2713@gmail.com>
2022-12-17 13:53:32 +0530

    Front-End & model

---
+++ src/components/Alert.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Alert() {
+  return (
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+     <strong>Vision Sehyog</strong> A.I and Computer Vision based application for the visually disabled, please grant camera accsess.
+     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+   </div>
+
+  )
+}
+
+export default Alert ;
---
+++ src/components/Output.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Output() {
+  return (
+    <div class="mb-3">
+     <label for="exampleFormControlTextarea1" class="form-label"><strong>Output From Our Medel</strong></label>
+     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+  )
+}
+
+export default Output;
